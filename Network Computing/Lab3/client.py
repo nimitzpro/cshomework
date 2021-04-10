5ada53a9e94f4b6c2a05f003ee5bbd8cd094376f
nimtizpro <astroalex5@gmail.com>
2021-04-10 12:08:54 +0100

    .

--- Network Computing/Lab3/client.py
+++ Network Computing/Lab3/client.py
@@ -1,39 +1,35 @@
-# from the socket module import all
 from socket import *
+import sys
+
+print(sys.argv)
+if len(sys.argv) != 4:
+    print("Please re-run with format 'py client.py <server_host> <server_port> <filename>'")
+    sys.exit()
+
+server_host = sys.argv[1]
+server_port = sys.argv[2]
+filename = sys.argv[3]
 
 sock = socket(AF_INET, SOCK_STREAM)
 
 HostName = gethostname()
 IP = gethostbyname(HostName)
-port = 6789
+print("client:", HostName, IP)
 
-print(HostName, IP)
-
-server_address = ("localhost", port)
+server_address = (server_host, int(server_port))
 print('connecting to server at %s port %s' % server_address)
 sock.connect(server_address)
 
-page = "localhost:"+ str(port) + "/HelloWorld.html"
-print(page)
-
-# """GET / HTTP/1.1
-# Host: %s
-# Connection: keep-alive
-# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0
-# Upgrade-Insecure-Requests: 1
-# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-# Accept-Language: en-US,en;q=0.5
-# Accept-Encoding: gzip, deflate"""
-
 try:
-    sock.send(("HEAD / HTTP/1.1\r\nHost: %s\r\nAccept: text/html" % page).encode())
+    sock.send(("Host: \\%s" % filename).encode())
 
-    amount_received = 0
-    while amount_received < 1024:
+    total = 0
+    data = " "
+    while data != "":
         data = sock.recv(1024).decode()
-        amount_received += len(data)
-        print('received "%s"' % data)
-        break
+        total += len(data)
+        print(data)
+        # break
 
 finally:
     print('closing socket')
